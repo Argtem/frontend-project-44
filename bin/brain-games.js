54e92482bd18bd74b75ee38b5a421a6422ebb174
artem <argtem.fury@gmail.com>
2025-12-21 19:13:50 +0300

    Попробую кое что

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,29 +1,4 @@
 #!/usr/bin/env node
 import { sayYourName } from '../src/cli.js'
-import { calcGame } from './brain-calc.js'
-import { evenGame } from './brain-even.js';
-import { primeGame } from './brain-prime.js';
-import { progressionGame } from './brain-progression.js';
-import { gcd } from './brain-gcd.js';
-import readlineSync from 'readline-sync';
 console.log("Welcome to the Brain Games!")
-function gameStart() {
-    let continueOrNot = 'yes';
-    let user = sayYourName()
-    let pickedGame = readlineSync.question()
-    if (pickedGame == "game-calc") {
-        calcGame(user)
-    } else if (pickedGame == "even-game") {
-        evenGame(user)
-    } else if (pickedGame == 'prime-even') {
-        primeGame(user)
-    } else if (pickedGame == "brain-gcd") {
-        gcd(user)
-    } else if (pickedGame == 'brain-progression') {
-        progressionGame(user)
-    } else if(pickedGame=="exit") {
-        continueOrNot="no"
-    }
-
-}
-gameStart()
+sayYourName()
